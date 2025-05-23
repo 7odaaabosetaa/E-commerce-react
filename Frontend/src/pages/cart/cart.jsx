01c11b6c29568d5433cc5a5307ef7889047748b7
7odaaabosetaa <7odaaabosetaa@gmail.com>
2025-05-23 23:12:15 +0200

    cart  page html & css

--- Frontend/src/pages/cart/cart.jsx
+++ Frontend/src/pages/cart/cart.jsx
@@ -4,6 +4,8 @@ import { useContext } from 'react';
 import shopContext from "../../componants/context/context";
 import allProducts from "../../Assets/all_product";
 import removeIcon from "../../Assets/cart_cross_icon.png"
+import { Link } from 'react-router-dom';
+import FooterShop from '../../componants/Footer';
 
 
 
@@ -11,14 +13,12 @@ import removeIcon from "../../Assets/cart_cross_icon.png"
 function Cart (){
 
     const {cartItems,removeFromCart} = useContext(shopContext);
-
-
     return (
         <div className='cart'>
             <div className="cartitems">
                 <div className="cartitems-format-main">
                     <p>products</p>
-                    <p>title</p>
+                    <p className='title'>title</p>
                     <p>price</p>
                     <p>Quintity</p>
                     <p>total</p>
@@ -29,14 +29,13 @@ function Cart (){
                {allProducts.map((e)=>{
                 if(cartItems[e.id] >0){
                     return <div>
-                       <div className='item'>
+                       <div className='cartitem'>
                         <img src={e.image} className='itemimg' alt="" />
-                        <p>e.name</p>
+                        <p className='title'>{e.name}</p>
                         <p>${e.new_price}</p>
-                        <button className='quintity'>{cartItems[e.id]}</button>
+                        <button className='quintity' onClick={()=>{}}>{cartItems[e.id]}</button>
                         <p>{e.new_price*cartItems[e.id]} </p>
-                            <img src={removeIcon} alt="" onClick={()=>{removeFromCart(e.id)}}/>
-                        
+                            <img className='remove' src={removeIcon} alt="" onClick={()=>{removeFromCart(e.id)}}/>
                     </div>
                     <hr />
                 </div>
@@ -46,7 +45,24 @@ function Cart (){
                 })}
 
             </div>
+            <div className='details'>
+                <div className="total">
+                    <h2>Cart total</h2>
+                    <p> <span>subtotal</span> <span>$70</span></p>
+                    <p> <span>shipping fee</span> <span>free</span></p>
+                    <p className='totalprice'> <h4>total</h4> <span>$70</span></p>
+                    <button> Peoceed to checkout</button>
+                </div>
+                <div className="promocode">
+                    <p>if you have promo code, Enter it here</p>
+                    <div>
+                        <input type="text" name="" placeholder='Promocode' />
+                        <button type="submit"> submit</button>
+                    </div>
+                </div>
 
+            </div>
+<FooterShop/>
         </div>
     );
 }
